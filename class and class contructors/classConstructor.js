6b8d88cec63422cb5079d2c7de5a6b48aa408d5d
Imanda03 <asis03ktm@gmail.com>
2022-08-09 15:46:18 +0545

    class contructor

---
+++ class and class contructors/classConstructor.js
@@ -0,0 +1,14 @@
+// Classess in javascript are like blueprint
+
+class User{
+    constructor(email,name){
+        this.email = email;
+        this.name = name;
+    }
+}
+
+var userOne = new User('asis03ktm','Anish');
+var userrTwo = new User('kakaNinja','Aashish');
+
+console.log(userOne)
+console.log(userrTwo)
--- objectLiterals/makingObjectLiterals.js
+++ objectLiterals/makingObjectLiterals.js
@@ -10,3 +10,23 @@ var userOne = {
 }
 
 console.log(userOne.name);
+console.log(userOne.email);
+
+
+
+//Updating properties
+userOne['name'] = 'Anish';
+userOne['email'] = 'asis03ninja.com';
+
+console.log(userOne.name);
+console.log(userOne.email);
+
+var pops = 'name';
+console.log(userOne[pops])
+
+//Creating properties
+userOne.age = 25;
+console.log(userOne.age);
+
+//creating function
+userOne.info = function(){}
