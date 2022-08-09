451fe8555426f2c71ae1dfe3402a9bf9128fb803
Imanda03 <asis03ktm@gmail.com>
2022-08-09 16:02:42 +0545

    method chaining

--- method chaining/methodChaining.js
+++ method chaining/methodChaining.js
@@ -2,17 +2,25 @@ class User {
     constructor(email, name) {
         this.name = name;
         this.email = email;
+        this.count = 0;
     }
     //Now this two are the methods
     login() {
         console.log(this.email, 'has been login');
+        return this;
     }
     logout() {
         console.log(this.email, ' has been logiout');
+        return this;
+    }
+    updateScore(){
+        this.count++;
+        console.log(this.email, 'count is ',this.count);
+        return this;
     }
 }
 var userOne = new User('asis03ktm', 'Anish');
 var userrTwo = new User('kakaNinja', 'Aashish');
 
-userOne.login();
-userrTwo.logout();
+//Now this is call chain methods
+userOne.login().updateScore().updateScore().logout();
